eaa5d0b4cfc08aa41ab87e424f59525c2b1b336b
DrVenter <ventercolin1@gmail.com>
2022-06-20 15:37:12 +0200

    still code vommit but added code to clean followers, following and posts data

--- clean_scraped_data_instagram_url_script.py
+++ clean_scraped_data_instagram_url_script.py
@@ -37,4 +37,51 @@ for x in range(len(matrix[heading_row])):
 
 matrix = np.delete(matrix, columns_to_remove, axis = 1)
 
-print(matrix)
+#create matrix for followers, following and posts
+placeholder = np.zeros((len(matrix), 3), dtype="S")
+
+placeholder[0] = ["Followers", "Following", "Posts"]
+
+description = find_heading_title("description")
+title = find_heading_title("title")
+
+def check_validity(string):
+    items_usualy_in_number = [".", ",", "k"]
+    k = 0
+    for x in items_usualy_in_number:
+        if x in string: string = string.replace(x, "")
+    
+    return string.isdigit()
+
+i = 1
+while i < len(matrix):
+    check_string = (str(matrix[i, description][0]) + " " + str(matrix[i, title][0])).replace("," , "").lower().split(" ")
+    keyword = ["followers", "following", "posts"]
+    row = ["", "", ""]
+
+    j = 0
+    while j < len(keyword): #need to change these stupid while loops to for loops
+        if keyword[j] in check_string: 
+            string_start = 0
+            string_end = len(check_string) - 1
+            count = check_string.count(keyword[j])
+
+            row[j] = check_string[check_string.index(keyword[j]) - 1]
+
+            while check_validity(row[j]) == False and string_start < string_end and count > 1:
+                string_start = check_string.index(keyword[j]) + 1
+                count = check_string[string_start:].count(keyword[j])
+
+                row[j] = check_string[check_string[string_start:].index(keyword[j]) - 1]
+                #print(row[j], check_string.count(keyword[j]))
+
+            if check_validity(row[j]) == False: row[j] = ""
+                
+                
+        else: 
+            row[j] = ""
+        j+=1
+    print(row)
+    i+=1
+
+#print(placeholder)
