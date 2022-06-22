ac4c6e78ceea482737283355fcc77eec7d02d257
DrVenter <ventercolin1@gmail.com>
2022-06-22 10:46:58 +0200

    cleaned code to be far more organized and converted to class

--- clean_scraped_data_instagram_url_script.py
+++ clean_scraped_data_instagram_url_script.py
@@ -1,120 +1,138 @@
 import numpy as np
 import pandas as pd
 
-#Open file and create matrix
-file_name = "raw_scraped_data_instagram_url.txt"
-file = open(file_name, errors="ignore")
+class CleanInstagram:
+    matrix = []
+    check_string_matrix = []
+    f_f_p = []
+    instagram_handles = []
+    email_address = []
 
-matrix = []
-for row in file:
-    matrix.append(row.strip("\n"))
-for column in range(len(matrix)):
-    matrix[column] = matrix[column].split("\t")
+    def __init__(self, file_name):
+        self.file_name = file_name
+        file = open(self.file_name, errors="ignore")
 
-matrix = np.array(matrix)
+        row_delimiter = "\n"
+        column_delimiter = "\t"
 
-#remove empty queries
-empty_queries = np.where(matrix[ : , 0] == "")
-matrix = np.delete(matrix, empty_queries, axis=0)
+        for row in file: self.matrix.append(row.strip(row_delimiter))
+        for column in range(len(self.matrix)): self.matrix[column] = self.matrix[column].split(column_delimiter)
+        
+        self.matrix = np.array(self.matrix)
 
+    def remove_empty_queries(self):
+        query_column = 0
 
-#find heading index
-def find_heading_title(title):
-    heading_row = 0
-    title_index_tuple = np.where(matrix[heading_row] == title)
-    title_index = title_index_tuple[0]
+        empty_queries = np.where(self.matrix[ : , query_column] == "")
+        self.matrix = np.delete(self.matrix, empty_queries, axis=0)
 
-    return title_index
+    def find_heading_index(self, heading_title):
+        heading_row = 0
 
-#remove unnecessary columns and place in correct order
-headings = ["query", "instagramUrl", "description", "title"]
-heading_indexes = list(map(find_heading_title, headings))
-heading_row = 0
-columns_to_remove = []
-for x in range(len(matrix[heading_row])):
-    if x not in heading_indexes:
-        columns_to_remove.append(x)
+        title_index_tuple = np.where(self.matrix[heading_row] == heading_title)
+        title_index = title_index_tuple[0]
 
-matrix = np.delete(matrix, columns_to_remove, axis = 1)
+        return title_index
 
-#create matrix for followers, following and posts
-#placeholder = np.zeros((len(matrix), 3), dtype=str)
+    def create_check_string_matrix(self):
+        description = self.find_heading_index("description")
+        title = self.find_heading_index("title")
+        inner_element = 0
+        heading_row = 0
+        title_description = [["title and description"]]
 
-placehold = [["Followers", "Following", "Posts"]]
+        for row in range(heading_row + 1, len(self.matrix)):
+            check_string = (str(self.matrix[row, description][inner_element]) + " " + str(self.matrix[row, title][inner_element])).replace("," , "").lower().split(" ")
+            title_description.append(check_string)
 
-description = find_heading_title("description")
-title = find_heading_title("title")
+        self.check_string_matrix = title_description
 
-def check_validity(string):
-    items_usualy_in_number = [".", ",", "k"]
-    k = 0
-    for x in items_usualy_in_number:
-        if x in string: string = string.replace(x, "")
+    def remove_unneeded_columns(self, heading_titles = ["query", "instagramUrl"]):
+        heading_indexes = list(map(self.find_heading_index, heading_titles))
+        heading_row = 0
+        columns_to_remove = []
+
+        for column in range(len(self.matrix[heading_row])):
+            if column not in heading_indexes: columns_to_remove.append(column)
+
+        self.matrix = np.delete(self.matrix, columns_to_remove, axis=1)
     
-    return string.isdigit()
+    def check_number_validity(self, string):
+        items_usually_in_numbers = [".", ",", "k"]
+        
+        for item in items_usually_in_numbers:
+            if item in string: string = string.replace(item, "")
+        
+        return string.isdigit()
 
-i = 1
-while i < len(matrix):
-    check_string = (str(matrix[i, description][0]) + " " + str(matrix[i, title][0])).replace("," , "").lower().split(" ")
-    keyword = ["followers", "following", "posts"]
-    row = ["", "", ""]
+    def create_follower_following_posts_matrix(self):
+        keywords = ["followers", "following", "posts"]
+        heading_row = 0
+        f_f_p = [["followers", "following", "posts"]]
 
-    j = 0
-    while j < len(keyword): #need to change these stupid while loops to for loops
-        if keyword[j] in check_string: 
-            string_start = 0
-            string_end = len(check_string) - 1
-            count = check_string.count(keyword[j])
+        for row in range(heading_row + 1, len(self.matrix)):
+            rows = ["", "", ""]
+            check_string = self.check_string_matrix[row]
 
-            row[j] = check_string[check_string.index(keyword[j]) - 1]
+            for keyword in range(len(keywords)):
+                if keywords[keyword] in check_string: 
+                    string_start = 0
+                    string_end = len(check_string) - 1
+                    count = check_string.count(keywords[keyword])
 
-            while check_validity(row[j]) == False and string_start < string_end and count > 1:
-                string_start = check_string.index(keyword[j]) + 1
-                count = check_string[string_start:].count(keyword[j])
+                    rows[keyword] = check_string[check_string.index(keywords[keyword]) - 1]
 
-                row[j] = check_string[check_string[string_start:].index(keyword[j]) - 1]
-                #print(row[j], check_string.count(keyword[j]))
+                    while self.check_number_validity(rows[keyword]) == False and string_start < string_end and count > 1:
+                        string_start = check_string.index(keywords[keyword]) + 1
+                        count = check_string[string_start:].count(keywords[keyword])
 
-            if check_validity(row[j]) == False: row[j] = ""
-        else: 
-            row[j] = ""
-        j+=1
+                        rows[keyword] = check_string[check_string[string_start:].index(keywords[keyword]) - 1]
+                    if self.check_number_validity(rows[keyword]) == False: rows[keyword] = ""
+
+                else: 
+                    rows[keyword] = ""
+            f_f_p.append(rows)
+        self.f_f_p = np.array(f_f_p)
+
+    def create_instagram_handle_email_matrix(self):
+        instagram_handles = [["Instagram Handles"]]
+        email_address = [["Email Addresses"]]
+        column_heading = 0
+
+        for row in range(column_heading + 1, len(self.matrix)):
+            check_string = self.check_string_matrix[row]
+            handles = ""
+            email = ""
+
+            for word in range(len(check_string)):
+                check_string[word] = check_string[word].strip("â€œ¢/()").replace("artists:", "")
+
+                if check_string[word].startswith("@"): handles = handles + " " + check_string[word].strip(".")
+                if "@" in check_string[word] and not check_string[word].startswith("@"): email = email + " " + check_string[word].strip(".")
+
+            handles = handles.strip().split()
+            handles = list(dict.fromkeys(handles))
+            handles = " ".join(handles)
+            
+            instagram_handles.append([handles])
+
+            email = email.strip()
+            email_address.append([email])
+        self.instagram_handles = np.array(instagram_handles)
+        self.email_address = np.array(email_address)
     
-    placehold.append(row)
-    i+=1
-    placeholder = np.array(placehold)
+    def combine_matrices(self):
+        self.matrix = np.concatenate((self.matrix, self.f_f_p, self.instagram_handles, self.email_address), axis=1)
 
+    def matrix_to_csv(self):
+        data_frame = pd.DataFrame(self.matrix)
+        data_frame.to_csv(f"cleaned_{self.file_name}.csv", index=False, header=False)
 
-#find instagram handles
-description = find_heading_title("description")
-title = find_heading_title("title")
-instagram_handles = [["Instagram Handles"]]
-email_address = [["email_address"]]
-
-for i in range(1, len(matrix)):
-    check_string = (str(matrix[i, description][0]) + " " + str(matrix[i, title][0])).replace("," , "").replace("(", "").replace(")", "").lower().split(" ")
-    handles = ""
-    email = ""
-
-    for x in range(len(check_string)):
-        #if check_string[x].endswith("."): check_string[x].strip(".")
-        check_string[x] = check_string[x].strip("â€œ¢/").replace("artists:", "")
-        if check_string[x].startswith("@"): handles = handles + " " + check_string[x].strip(".")
-        if "@" in check_string[x] and not check_string[x].startswith("@"): email = email + " " + check_string[x].strip(".")
-    handles = handles.strip()
-    instagram_handles.append(handles)
-
-    email = email.strip()
-    email_address.append(email)
-
-print(email_address)
-
-#combine matrices
-matrix = np.concatenate((matrix, placeholder), axis=1)
-
-#save matrix as data fram and file
-data_frame = pd.DataFrame(matrix)
-data_frame.to_csv(f"cleaned_{file_name}.csv", index=False, header=False)
-
-
-#print(placeholder)
+data = CleanInstagram("scraped_data_instagram_url.txt")
+data.remove_empty_queries()
+data.create_check_string_matrix()
+data.remove_unneeded_columns()
+data.create_follower_following_posts_matrix()
+data.create_instagram_handle_email_matrix()
+data.combine_matrices()
+data.matrix_to_csv()
